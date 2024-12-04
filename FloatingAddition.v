d788a8ac2a708b513d9fca030ab1c4f28008dee8
ashollerbach <100439062+ashollerbach@users.noreply.github.com>
2024-12-04 17:00:55 -0600

    stuff
    Replaced while loop because it would not synthesize

--- FloatingAddition.v
+++ FloatingAddition.v
@@ -22,43 +22,49 @@ reg [22:0] FinalMantissa;                   //mantissa of final result
 reg [7:0] FinalExponent;                    //exponent of final result
 reg FinalSign;                              //sign of final result
 
-    
+        // Replacing the while loop with a for loop to shift until the leading bit is 1
+        reg flag;
+        integer counter;
+
 always @(*) begin
-    comp =  (A[30:23] >= B[30:23])? 1'b1 : 1'b0; //if a is equal to or bigger than b, comp is 1, else comp is 0
-      
-    LargerMantissa = comp ? {1'b1,A[22:0]} : {1'b1,B[22:0]};    //if comp is 1, the large mantissa is the mantissa of input a. Else, the large mantissa is the mantissa of input b
-    LargerExponent = comp ? A[30:23] : B[30:23];                //if comp is 1, the large exponent is the exponent of input a. Else, the large exponent is the exponent of input b
-    LargerSign = comp ? A[31] : B[31];                          //if comp is 1, the large sign is the exponent of sign a. Else, the large sign is the sign of input b
-      
+    comp =  (A[30:23] >= B[30:23])? 1'b1 : 1'b0; //if A is equal to or bigger than B, comp is 1, else comp is 0
+       
+    LargerMantissa = comp ? {1'b1, A[22:0]} : {1'b1, B[22:0]};    //if comp is 1, the large mantissa is A. Else, it's B
+    LargerExponent = comp ? A[30:23] : B[30:23];                //if comp is 1, the large exponent is A. Else, it's B
+    LargerSign = comp ? A[31] : B[31];                          //if comp is 1, the large sign is A. Else, it's B
+       
     SmallerMantissa = comp ? {1'b1, B[22:0]} : {1'b1, A[22:0]};
     SmallerExponent = comp ? B[30:23] : A[30:23];
     SmallerSign = comp ? B[31] : A[31];
     
     ExponentDifference = LargerExponent - SmallerExponent; //difference in exponents
-    SmallerMantissa = (SmallerMantissa >> ExponentDifference); //shift small number mantissa by the difference in exponents to align the mantissas
-    {carry,TemporaryMantissa} = (LargerSign ~^ SmallerSign)? LargerMantissa + SmallerMantissa : LargerMantissa - SmallerMantissa; //if the signs are the same, add the mantissas. Else, subtract the mantissas. This is how we decide to add or subtract.
+    SmallerMantissa = (SmallerMantissa >> ExponentDifference); //shift smaller mantissa by the difference in exponents to align
     
-    TemporaryExponent = LargerExponent; //TemporaryExponent is the larger exponent for now, but we will operate on it
+    {carry, TemporaryMantissa} = (LargerSign ~^ SmallerSign) ? LargerMantissa + SmallerMantissa : LargerMantissa - SmallerMantissa; //add or subtract mantissas based on signs
     
-    //the goal of the following code is to shift the mantissa so that the leading bit is a 1
-    if(carry) //If there is a carry-out, shift the result right and increment the exponent
-        begin
-            TemporaryMantissa = TemporaryMantissa >> 1;
-            TemporaryExponent = TemporaryExponent + 1'b1;
+    TemporaryExponent = LargerExponent; //TemporaryExponent is initially set to the larger exponent
+    
+    // The goal of the following code is to shift the mantissa so that the leading bit is a 1
+    if (carry) begin // If there is a carry-out, shift right and increment the exponent
+        TemporaryMantissa = TemporaryMantissa >> 1;
+        TemporaryExponent = TemporaryExponent + 1'b1;
+    end else begin
+
+        // Initialize flag based on the leading bit of TemporaryMantissa
+        flag = TemporaryMantissa[23];
+
+        // Use a for loop up to 24 shifts
+        for (counter = 0; counter < 24 && !flag; counter = counter + 1) begin
+            TemporaryMantissa = TemporaryMantissa << 1;
+            TemporaryExponent = TemporaryExponent - 1'b1;
+            flag = TemporaryMantissa[23]; // Update the flag after shifting
         end
-    else
-        begin
-        while(!TemporaryMantissa[23]) //Otherwise, left-shift the result until the leading bit is 1 and adjust the exponent
-            begin
-               TemporaryMantissa = TemporaryMantissa << 1;
-               TemporaryExponent =  TemporaryExponent - 1'b1;
-            end
-        end
+    end
     
-    //final result creation    
+    // Final result creation    
     FinalSign = LargerSign;
     FinalExponent = TemporaryExponent;
     FinalMantissa = TemporaryMantissa[22:0];
-    result = {FinalSign,FinalExponent,FinalMantissa};
-    end
+    result = {FinalSign, FinalExponent, FinalMantissa};
+end
 endmodule
